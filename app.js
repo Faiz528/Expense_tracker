c3b07c32fbe5c28a0fa9d6c54f2a56ed736eaaf9
MD FAIZUDDIN <84907848+Faiz528@users.noreply.github.com>
2023-05-23 15:22:37 +0530

    user table linked

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const cors = require('cors')
 const Parser = require('body-parser')
 const sequelize = require('./util/database')
-
+const Track = require('./models/track')
+const User = require('./models/user')
 
 app.use(cors())
 app.use( Parser.json({extended :false}))
@@ -11,13 +12,28 @@ app.use( Parser.json({extended :false}))
 const expenseRoute = require('./routes/track')
 
 app.use(expenseRoute)
-
-  sequelize
-    .sync()
+app.use((req,res,next)=>{
+  User.findByPk(1).then(user=>{
+    req.user=user;
+    next();
+  })
+})
+ 
+Track.belongsTo(User,{constraint:true,OnDelete:'CASCADE'})
+User.hasMany(Track)
+   sequelize
+    .sync({force:true})
     .then(result => {
-     app.listen(3000)
-    })
-    
+      return User.findByPk(1)
+    }).then(user=>{
+      if(!user){
+        User.create({Name:"Faiz", Email:'mahmood@gmail.com'})
+      }
+      return user
+      }).then(user=>{
+        console.log(user)
+        app.listen(3000)
+      })
     .catch(err => {
       console.log(err);
     });
